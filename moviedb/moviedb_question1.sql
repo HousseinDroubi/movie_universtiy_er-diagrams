bf0a1b3320e5f2164daf8a8bcef0be86c60e8301
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-11 17:46:41 +0300

    Find requirments with all the attributes of both tables

--- moviedb/moviedb_question1.sql
+++ moviedb/moviedb_question1.sql
@@ -1,3 +1,3 @@
-SELECT * 
-FROM movie INNER JOIN oscar ON movie.id = oscar.movie_id 
-WHERE oscar.type="Best-Picture";
+SELECT * FROM 
+movie INNER JOIN oscar ON movie.id = oscar.movie_id 
+WHERE oscar.type="Best-Picture" AND movie.earnings_rank = (SELECT MIN(movie.earnings_rank) FROM movie WHERE 1) LIMIT 1;
