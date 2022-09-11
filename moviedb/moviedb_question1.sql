55ec41f73d4127e106ebacc5e222459538c4473d
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-11 17:50:27 +0300

    Finish Question 1

--- moviedb/moviedb_question1.sql
+++ moviedb/moviedb_question1.sql
@@ -1,3 +1,4 @@
-SELECT * FROM 
-movie INNER JOIN oscar ON movie.id = oscar.movie_id 
-WHERE oscar.type="Best-Picture" AND movie.earnings_rank = (SELECT MIN(movie.earnings_rank) FROM movie WHERE 1) LIMIT 1;
+SELECT movie.name,movie.earnings_rank 
+FROM movie INNER JOIN oscar ON movie.id = oscar.movie_id 
+WHERE oscar.type="Best-Picture" AND movie.earnings_rank = (SELECT MIN(movie.earnings_rank) FROM movie WHERE 1) 
+LIMIT 1;
